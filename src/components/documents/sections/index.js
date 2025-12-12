5052a2d999b977ef72410ead1504f32a4f9646f2
Raju <drraj@localhost>
2025-12-12 22:52:24 +0400

    restore invoice

--- src/components/documents/sections/index.js
+++
@@ -1,9 +0,0 @@
-// Barrel export file for document section components
-// Provides clean imports: import { HeaderSection, PartySection } from './sections'
-
-export { default as HeaderSection } from './HeaderSection';
-export { default as PartySection } from './PartySection';
-export { default as LineItemsSection } from './LineItemsSection';
-export { default as ChargesTotalsSection } from './ChargesTotalsSection';
-export { default as NotesSection } from './NotesSection';
-export { default as ActionsBar } from './ActionsBar';
